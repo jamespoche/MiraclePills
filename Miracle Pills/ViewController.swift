d380d58cbfb1a018849a06d7a4a94d981a16b1f8
James PY Chen <jamespoyu@gmail.com>
2017-08-14 14:21:30 +0800

    first commit

---
+++ Miracle Pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miracle Pills
+//
+//  Created by jamespoyu on 2017/8/13.
+//  Copyright © 2017年 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.view.backgroundColor = UIColor.purple
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
